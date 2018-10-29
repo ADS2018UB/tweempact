a8b66c3764409fb05927d3084fad57b3250f6fbf
praat6 <43272208+praat6@users.noreply.github.com>
2018-10-29 23:00:47 +0100

    #14

---
+++ scrapping/Albert/balanced_v2.py
@@ -0,0 +1,25 @@
+def balanced(df):
+	import datetime,time
+	import numpy as numpy
+	import pandas as pd
+	
+	cleaned = df[["id", "created_at","entities", "extended_entities","favorite_count","retweet_count","source","text", "truncated","user","quoted_status_id"]]
+	ids = [user["id"] for user in cleaned["user"]]
+	cleaned["id_num"] = ids
+
+	for _id in ids:
+		subset = cleaned[cleaned["id_num"]==_id]
+		subset = subset.reset_index(drop=True)
+		reindexed = subset["created_at"]
+		reindexed = subset["created_at"].reset_index(drop=True)
+		#convert tweet date into python date
+		subset["created_at_pydate"] = [time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(reindexed[i],'%a %b %d %H:%M:%S +0000 %Y'))for i in range(0,len(reindexed))]
+		subset = subset.sort_values(by="created_at_pydate")
+		last10_tweets = subset[-10:]
+		#mean last 10 tweets
+		RT_l10 = np.mean(last10_tweets["retweet_count"])
+		FC_l10 = np.mean(last10_tweets["favorite_count"])
+		#take json user
+		j_user = subset[-1:]["user"][0]
+		
+	return 
