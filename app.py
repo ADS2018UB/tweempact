9a58632469e4ba302154aa772dd99609b08a6a0c
Ramon <ramon4mm@gmail.com>
2018-12-15 19:43:16 +0100

    solving the hashtags issue #65

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def prediction_made(text):
         w = w.strip('#') 
         has+= w
         has+= ','
-            
+    has = has[:-1]        
     # text = request.args.get('text')
     # some response showing the number of RT/FAVS
     return render_template('prediction/aftermath.html', RT=RT_mean, FAV=FAV_mean, text=text, RTaa=RT_text, FAVaa=FAV_text,has = has)
