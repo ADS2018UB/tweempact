7200738cb70c1894452fb9626e2eaa1c633be43d
Ramon <ramon4mm@gmail.com>
2018-12-20 00:34:13 +0100

    last change
    eureka

--- web-app/app/functions.py
+++ web-app/app/functions.py
@@ -247,7 +247,7 @@ def predict(df):
         if (np.mean(RT) > mu_RT + sd_RT*100)[0] or (np.mean(RT) < mu_RT - sd_RT*100)[0]:
             RT = seg.intervals[k]
 
-    elif FC_class==2: 
+    elif RT_class==2: 
         X  = load_model('trained_vect_RT_C2.sav').transform(df_tweet).todense()
         RT = seg.intervals[load_model('trained_RT_C2_tweet.sav').predict(X)[0]]
         if (np.mean(RT) > mu_RT + sd_RT*100)[0] or (np.mean(RT) < mu_RT - sd_RT*100)[0]:
